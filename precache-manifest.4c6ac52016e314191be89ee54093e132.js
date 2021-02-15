74b163bbb347a4ced899e481722d7d302a8e66ed
jeremy-habit <jeremy.habit@gmail.com>
2021-02-15 01:38:58 +0100

    Updates

---
+++ precache-manifest.4c6ac52016e314191be89ee54093e132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c62dc27cc8e029ba2a8b3b004ff602",
+    "url": "/react-Jeux-Oxo/index.html"
+  },
+  {
+    "revision": "aabf4008b65d6601b105",
+    "url": "/react-Jeux-Oxo/static/css/main.d8b81d22.chunk.css"
+  },
+  {
+    "revision": "c3e771bb88fc924f4c82",
+    "url": "/react-Jeux-Oxo/static/js/2.2d9d0d98.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/react-Jeux-Oxo/static/js/2.2d9d0d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabf4008b65d6601b105",
+    "url": "/react-Jeux-Oxo/static/js/main.af7b086f.chunk.js"
+  },
+  {
+    "revision": "23a9c443068dea93a977",
+    "url": "/react-Jeux-Oxo/static/js/runtime-main.212ba120.js"
+  }
+]);
